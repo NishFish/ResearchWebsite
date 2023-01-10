cf2134bd01d249bb2217fab2bbe9fdb1fa1cceb5
NishFish <ngopinat@buffalo.edu>
2023-01-09 20:49:37 -0500

    changed to flask framework

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
-import json
-import socketserver
+from flask import Flask, render_template, send_file, request, url_for, redirect, abort, make_response
 from pymongo import MongoClient
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    clients = []
+app = Flask(__name__)
 
-    def handle(self):
-        received_data = self.request.recv(2048) #Number of bytes to receive
-        if (len(received_data) > 0): #to avoid empty bytes
-            split = received_data.split(b'\r\n\r\n', 1)  # seperate headers and body in bytes
-            decodedContent = split[0].decode()  # decode headers ONLY
-            store = list(decodedContent.split("\r\n"))  # split headers by newlines
-            request = str(store[0]).split(" ")  # eg. ['GET', '/style.css', 'HTTP/1.1']
-            if(request[1] == "/"):
-                opened = open("index.html", 'rb').read()
-                length = len(bytes(opened))
-                self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: " + str(length) + "\r\n\r\n").encode() + opened)
-            elif(request[1] == "/style.css"):
-                opened = open(request[1][1:], 'rb').read()
-                length = len(bytes(opened))
-                self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: " + str(length) + "\r\n\r\n").encode() + opened)
-            elif(request[1][:8] == "/images/"):
-                opened = open(request[1][1:], 'rb').read()
-                length = len(bytes(opened))
-                self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Type: image/png; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: " + str(length) + "\r\n\r\n").encode() + opened)
-            elif(request[1] == "/teampage.html"):
-                opened = open("teampage.html", 'rb').read()
-                length = len(bytes(opened))
-                self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: " + str(length) + "\r\n\r\n").encode() + opened)
+@app.route('/')
+def default_page():
+    return render_template('index.html', mimetype="text/html")
+
+@app.route('/style.css')
+def default_page_style():
+    return send_file('style.css', mimetype="text/css")
+
+@app.route('/teampage.html')
+def team_page():
+    return render_template('teampage.html', mimetype="text/html")
+
+@app.route('/<image_name>')
+def display_image(image_name):
+    return send_file('images/' + image_name, mimetype="image/gif")
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = 8083
-
-    socketserver.TCPServer.allow_reuse_port = True
-    server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler) #used to start server
-    server.serve_forever()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8083)
