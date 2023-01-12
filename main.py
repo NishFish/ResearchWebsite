bd8621cb84aeba38fd85be3f78befd7fb1ed1aa0
Dima <Dmitrijsnaudzuns@gmail.com>
2023-01-12 17:45:31 -0500

    Added video upload and thankyou message

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, send_file, request, url_for, redirect, abort, make_response
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def default_page():
     if request.method == 'POST':
-       video = request.files['file'] 
+        video = request.files['file']
+        if video.filename == '':
+            print('File name is invalid') 
+            return redirect(request.url)
+        filename = secure_filename(video.filename)
     return render_template('index.html', mimetype="text/html")
 
 @app.route('/style.css')
@@ -21,7 +27,9 @@ def team_page():
 def display_image(image_name):
     return send_file('images/' + image_name, mimetype="image/gif")
 
-
+@app.route('/script.js')
+def default_page_script():
+    return send_file('templates/script.js', mimetype='text/javascript')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8083)
---
+++ templates/script.js
@@ -0,0 +1,4 @@
+console.log('working')
+function alertUser(){
+    alert('Thanks for submitting your video!')
+}
